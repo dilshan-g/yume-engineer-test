fef5b49d715c92ec8e4fbccdb8599f2c49652a74
Dilshan Gunasekra <nibmdilshan@gmail.com>
2024-03-24 19:06:58 +1100

    test: Added unit tests for the validation failure scenarios

--- packages/back-end/tests/Unit/ProductTest.php
+++ packages/back-end/tests/Unit/ProductTest.php
@@ -122,4 +122,89 @@
             $this->assertDatabaseMissing('products', ['id' => $this->product->id]);
         }
 
+        /**
+         * Fails the request when the product name is given empty.
+         *
+         * @return void
+         */
+        public function test_unsuccessful_response_on_product_name_is_empty(): void
+        {
+            $response = $this->json('POST', 'api/products', $this->empty_product_name);
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The name field is required.',
+            ]);
+
+            $response = $this->json('PUT', 'api/products/' . $this->product->id, $this->empty_product_name);
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The name field is required.',
+            ]);
+        }
+
+        /**
+         * Fails the request when the product name is less than 3 characters short.
+         *
+         * @return void
+         */
+        public function test_unsuccessful_response_on_product_name_length_too_short(): void
+        {
+            $response = $this->json('POST', 'api/products', $this->short_product_name);
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The name field must be at least 3 characters.',
+            ]);
+
+            $response = $this->json('PUT', 'api/products/' . $this->product->id, $this->short_product_name);
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The name field must be at least 3 characters.',
+            ]);
+        }
+
+        /**
+         * Fails the request when the product ID does not exists.
+         *
+         * @return void
+         */
+        public function test_unsuccessful_response_on_deleting_an_invalid_product_id(): void
+        {
+            $response = $this->json('GET', 'api/products/2');
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The Product not found.'
+            ]);
+
+            $response = $this->json('DELETE', 'api/products/2');
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'The Product not found.'
+            ]);
+        }
+
+        /**
+         * Fails the request when the product ID is not a number.
+         *
+         * @return void
+         */
+        public function test_unsuccessful_response_on_product_id_is_not_a_number(): void
+        {
+            $response = $this->json('GET', 'api/products/2h');
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'Product ID must be an Integer.'
+            ]);
+
+            $response = $this->json('PUT', 'api/products/2h', $this->update_product_data);
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'Product ID must be an Integer.'
+            ]);
+
+            $response = $this->json('DELETE', 'api/products/2h');
+            $response->assertStatus(422);
+            $response->assertJson([
+                'message' => 'Product ID must be an Integer.'
+            ]);
+        }
     }
